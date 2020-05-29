e5239cb7629870cd7199cf8dbe313bd446b90e04
Saksham Kapur <saksham@sutratech.io>
2020-05-30 03:06:35 +0530

    Updates

---
+++ precache-manifest.c3b6e86c30380dd87e7a0e68360fdef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c74fd8abbc826c6e6429e0f639db8d",
+    "url": "/Sakshamkapur/cropie/index.html"
+  },
+  {
+    "revision": "10316fce5c821a289215",
+    "url": "/Sakshamkapur/cropie/static/css/main.3b0c3e93.chunk.css"
+  },
+  {
+    "revision": "fc3cc38685746aaccdf3",
+    "url": "/Sakshamkapur/cropie/static/js/2.bb6027da.chunk.js"
+  },
+  {
+    "revision": "62b911716463a3d0cf54fc81855cea2d",
+    "url": "/Sakshamkapur/cropie/static/js/2.bb6027da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10316fce5c821a289215",
+    "url": "/Sakshamkapur/cropie/static/js/main.9c913f25.chunk.js"
+  },
+  {
+    "revision": "2759f1cd3701fea1b961",
+    "url": "/Sakshamkapur/cropie/static/js/runtime-main.1627923a.js"
+  }
+]);
